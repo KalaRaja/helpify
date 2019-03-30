f54e7fe3cce90e1aad61f74dbdffd931f85563cf
arpibhat <arpibhat@iu.edu>
2019-03-30 17:00:38 -0400

    login and register

---
+++ config/database.php
@@ -0,0 +1,27 @@
+<?php
+/*
+ * Caution: Need to call $conn->close() after including this file
+ */
+
+// $db_server = 'localhost';
+// $db_username = 'postgres';
+// $db_password = "Winteriscoming20!";
+// $db_name = 'infoarch';
+// $conn = new pg_co($db_server, $db_username, $db_password, $db_name) or die('Unable to connect to Database');
+$conn = pg_connect("host=localhost port=5432 user = postgres password=Winteriscoming20! dbname=infoarch");
+
+
+function sendQuery($cmd) {
+    global $conn;
+    try{
+    
+    $result = pg_query($conn,$cmd);
+  
+    }
+    catch(Exception $e) {
+        echo 'Message: ' .$e->getMessage();
+      }
+
+
+    
+}
