b98d8bea9918a0ec270492b68651655ae3e19280
kalaraja <shekhar957@gmail.com>
2019-03-31 20:54:00 -0400

    added styles and other cards

---
+++ js/timeline.js
@@ -0,0 +1,38 @@
+
+function registerClicks() {
+    $("#menu-toggle").click(function(e) {
+        console.log('clicked');
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+     });
+     $("#menu-toggle-2").click(function(e) {
+        console.log('clicked');
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled-2");
+        $('#menu ul').hide();
+     });
+}
+
+ function initMenu() {
+     console.log(1);
+    $('#menu ul').hide();
+    $('#menu ul').children('.current').parent().show();
+    //$('#menu ul:first').show();
+    $('#menu li a').click(
+       function() {
+          var checkElement = $(this).next();
+          if ((checkElement.is('ul')) && (checkElement.is(':visible'))) {
+             return false;
+          }
+          if ((checkElement.is('ul')) && (!checkElement.is(':visible'))) {
+             $('#menu ul:visible').slideUp('normal');
+             checkElement.slideDown('normal');
+             return false;
+          }
+       }
+    );
+ }
+ $(document).ready(function() {
+    registerClicks()
+    initMenu();
+ });
