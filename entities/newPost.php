6988b6de7835e51a23f624e3daf3d04d5b9e3fb8
arpibhat <arpibhat@iu.edu>
2019-04-19 18:58:42 -0400

    newPost web service added

--- entities/newPost.php
+++ entities/newPost.php
@@ -9,18 +9,31 @@ $address = $_GET['address'];
 $email = $_GET['email'];
 $starttime = $_GET['starttime'];
 $endtime = $_GET['endtime'];
+
 $imageurl = $_GET['imageurl'];
+// $arrayImageUrl = array();
+$arrayImageUrl = explode(',',$imageurl);
 
 $conn = pg_connect("host=localhost port=5432 user = postgres password=Winteriscoming20! dbname=infoarch");
+$insert_posts = pg_query($conn, "INSERT INTO posts VALUES 
+                                (DEFAULT, '$content', '$email', '$tagid', 'False', '$address', '$starttime', '$endtime')");
 
 $getMaxPid = pg_query($conn, "Select max(pid) from posts");
 $row = pg_fetch_row($getMaxPid);
-$currentPid = $row[0] +1;
-$insert_posts_images = pg_query($conn, "INSERT INTO posts_images VALUES 
-                                        ($currentPid, '$content')");// Doubt: how to get number of images that needs to be inserted
+$currentPid = $row[0];
+$noOfImages =0;
+if(is_array($arrayImageUrl)){
+    $noOfImages = sizeof($arrayImageUrl);
+}
 
-$insert_posts = pg_query($conn, "INSERT INTO posts VALUES 
-                                (DEFAULT, '$content', '$email', '$tagid', 'False', '$address', '$starttime', '$endtime')");
+
+for($x=0; $x < $noOfImages; $x++)
+{
+
+ $insert_posts_images = pg_query($conn, "INSERT INTO post_images VALUES 
+                                        ($currentPid, '$arrayImageUrl[$x]')");
+}
+
 
 $postobject = isset($postobject) ? $postobject : new stdClass();
 $postobject->NewPost='Success';
