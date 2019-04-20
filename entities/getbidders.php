8d83650bb7eb6ce537a827f5710180b775b87303
nishchayagupta <nishchayagupta@gmail.com>
2019-04-20 18:55:53 -0400

    get bidders module added

---
+++ entities/getbidders.php
@@ -0,0 +1,22 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-type: application/json');
+
+$pid = $_GET['pid'];
+
+$conn_string = "host=localhost dbname=postgres port=5432 user=postgres password=Naughty880042";
+
+$dbconn = pg_connect($conn_string);
+
+$query = pg_query($dbconn, "SELECT * from getBiddersInfo($pid)");
+
+$return_array = array();
+while ($row = pg_fetch_assoc($query)) {
+    $object = (object)['first_name' => $row['first_name'], 'last_name' => $row['last_name'], 'imageurl' => $row['imageurl'], 'rating' => $row['rating']];
+    // $json_object = json_encode($object);
+    array_push($return_array, $object);      
+}
+
+echo json_encode($return_array);
+
+?>
